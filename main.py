dc98c38ad0d65ad4dc5dd85dd21620de574cc646
Mayuresh Bhandari <102815435+mayur1710@users.noreply.github.com>
2025-09-05 20:28:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
-#  MIT License
-#
-#  Copyright (c) 2019-present Dan <https://github.com/delivrance>
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#  SOFTWARE
-
-
 import logging
 import asyncio
 from logging.handlers import RotatingFileHandler
@@ -38,15 +15,20 @@ logging.basicConfig(
     ],
 )
 
+# Your API keys (from https://my.telegram.org)
+API_ID = 25519039
+API_HASH = "1890ea8e01f2824e5827ee07cb6c51d3"
+
 # Bot token (from BotFather)
 BOT_TOKEN = "8413663158:AAGWgLfNUji2l7weqw5JODMoA9ftL9XBWYo"
 
-# Plugin directory
 plugins = dict(root="plugins")
 
 if __name__ == "__main__":
     bot = Client(
         "StarkBot",
+        api_id=API_ID,
+        api_hash=API_HASH,
         bot_token=BOT_TOKEN,
         plugins=plugins,
         workers=50
@@ -60,3 +42,4 @@ if __name__ == "__main__":
 
     asyncio.get_event_loop().run_until_complete(main())
     LOGGER.info("<--- Bot Stopped --->")
+
